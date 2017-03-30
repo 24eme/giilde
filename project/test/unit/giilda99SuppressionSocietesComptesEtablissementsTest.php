3670b267294965e018ffde2504ff40a6eb0bb3da
Vincent LAURENT <vince.laurent@gmail.com>
2017-03-30 16:08:13 +0200

    Mise à jour du nombre de test dans la suppression

--- project/test/unit/giilda99SuppressionSocietesComptesEtablissementsTest.php
+++ project/test/unit/giilda99SuppressionSocietesComptesEtablissementsTest.php
@@ -7,7 +7,7 @@ if (getenv("NODELETE")) {
     exit(0);
 }
 
-$t = new lime_test(($application == "bivc") ? 35 : 32);
+$t = new lime_test(($application == "bivc") ? 38 : 35);
 
 $t->comment('suppression des différentes sociétés, de leurs établissements et comptes');
 
