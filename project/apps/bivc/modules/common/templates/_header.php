d0a4a7384897cd4af9576351a2a2e25039cc2b62
Tangui Morlier <tangui@tangui.eu.org>
2017-06-09 19:27:32 +0200

    Tentative de stype pour le BIVC

--- project/apps/bivc/modules/common/templates/_header.php
+++ project/apps/bivc/modules/common/templates/_header.php
@@ -1,6 +1,12 @@
-<nav class="navbar navbar-default navbar-static-top">
-     <div class="container" style="position: relative;">
-        <img src="/images/logo_site_bivc.svg" style="height: 46px; position: absolute; left: -44px; padding-top: 4px;" />
+<header id="header">
+	<div class="header-top">
+		<div class="container">
+			<img class="logo-site" src="/images/logo_site_bivc.svg"/>
+		</div>
+	</div>
+    <nav class="navbar navbar-default navbar-static-top">
+    	<div class="container">
         <?php include_component('common', 'nav'); ?>
-    </div>
-</nav>
+    	</div>
+	</nav>
+</header>
